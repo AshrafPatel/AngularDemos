e635112db8fe1c9af9ecd4b22e501d341044cdb3
Ashraf Patel <4shr4f@live.co.uk>
2019-10-03 00:31:39 -0400

    Added sccrimba project

---
+++ scrimba-demo/src/app/customers/customers-list/customers-list.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customers-list',
+  templateUrl: './customers-list.component.html',
+  styleUrls: ['./customers-list.component.css']
+})
+
+export class CustomersListComponent implements OnInit {
+  ngOnInit() {
+
+  }
+}
